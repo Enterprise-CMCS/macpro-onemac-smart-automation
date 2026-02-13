906627436b4f6f8763d1678a39911ad9f70cedfd
Ammar <testautomationnotifications@gmail.com>
2026-02-12 20:57:52 -0500

    fixed email issue

--- src/main/java/gov/cms/smart/utils/reporting/ExtentReportManager.java
+++ src/main/java/gov/cms/smart/utils/reporting/ExtentReportManager.java
@@ -2,17 +2,22 @@ package gov.cms.smart.utils.reporting;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.JsonFormatter;
 
 public class ExtentReportManager {
     private static ExtentReports extent;
 
     public static ExtentReports getInstance() {
         if (extent == null) {
+            extent = new ExtentReports();
+            // 1. Setup HTML Report (Spark)
             ExtentSparkReporter spark = new ExtentSparkReporter("extent-report/OneMAC-SMART-TestReport.html");
             spark.config().setReportName("OneMAC SMART Automated Test Report");
             spark.config().setDocumentTitle("Test Results");
-            extent = new ExtentReports();
-            extent.attachReporter(spark);
+            // 2. Setup JSON Report
+            JsonFormatter json = new JsonFormatter("extent-report/OneMACTestReport.json");
+            // 3. Attach both reporters
+            extent.attachReporter(spark, json);
         }
         return extent;
     }
